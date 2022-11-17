6a457cf850572b751cb35daf46c4999dca253300
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-11-17 11:11:24 -0300

    FollowButton changes, fixed

--- src/components/Buttons/FollowButton.jsx
+++ src/components/Buttons/FollowButton.jsx
@@ -5,8 +5,8 @@ const FollowButton = ({
 }) => {
 
   return (
-    <div className={`${following ? 'bg-red-500 px-3 py-1 text-sm cursor-pointer' : ' px-7 rounded-sm py-1 my-2  cursor-pointer'} `} >
-      <p className="text-white bg-blue-400 py-1 rounded-sm px-7">{following ? 'unfollow' : 'Follow'}</p>
+    <div className={`${following ? 'px-3 py-1 text-sm cursor-pointer' : ' px-7 rounded-sm py-1 my-2  cursor-pointer'} `} >
+      <p className={`text-white  py-1 rounded-sm px-7 ${following ? 'bg-red-500' : 'bg-blue-400'}`}>{following ? 'unfollow' : 'Follow'}</p>
     </div>
   )
 
